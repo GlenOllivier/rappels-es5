d94813fd29faa2282d5b51c0d3266ec43fdcb7b1
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-07-09 11:32:31 +0200

    01 - Function

--- 01.function.js
+++ 01.function.js
@@ -15,3 +15,21 @@ var somme = additionner;
 var resultat2 = somme(nb1, nb2);
 
 console.log("resultat2 =", resultat2);
+
+var multiplication = function (nb1, nb2) {
+    return nb1 * nb2;
+}
+
+var resultat3 = multiplication(nb1, nb2);
+
+console.log("resultat3 =", resultat3);
+
+var afficherOperation = function(nom, operation, nb1, nb2) {
+    console.log(nom + "(" + nb1 + "," + nb2 + ") = " + operation(nb1, nb2));
+}
+
+afficherOperation("Somme", somme, 20, 40);
+afficherOperation("Multiplication", multiplication, 10, 20);
+afficherOperation("Soustraction", function(nb1, nb2) {
+    return nb1 - nb2;
+}, 15, 5);
